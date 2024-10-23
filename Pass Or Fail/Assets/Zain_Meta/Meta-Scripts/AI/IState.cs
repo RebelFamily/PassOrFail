6bbebb48d56bd8cdb2061277add4173e29cd2739
Zain Ul Abideen <zainiftikhar@gamedistrict.co>
2024-10-23 16:05:45 +0500

    State Pattern Base

---
+++ Pass Or Fail/Assets/Zain_Meta/Meta-Scripts/AI/IState.cs
@@ -0,0 +1,9 @@
+﻿namespace Zain_Meta.Meta_Scripts.AI
+{
+    public interface IState
+    {
+        public void EnterState(StudentStateManager stateManager);
+        public void ExitState(StudentStateManager stateManager);
+        public void UpdateState(StudentStateManager stateManager);
+    }
+}
---
+++ Pass Or Fail/Assets/Zain_Meta/Meta-Scripts/AI/StudentStateManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Zain_Meta.Meta_Scripts.AI
+{
+    public class StudentStateManager : MonoBehaviour
+    {
+        private IState _curState;
+
+        private void LateUpdate()
+        {
+            _curState?.UpdateState(this);
+        }
+
+        public void ChangeState(IState newState)
+        {
+            if (_curState != null)
+            {
+                if (_curState == newState) return;
+                _curState.ExitState(this);
+            }
+
+            _curState = newState;
+            _curState.EnterState(this);
+        }
+    }
+}
